ce108c92240185f9982a1b0aa68557d4a811ec43
Ariel Himmelstern <arhimmel@gmail.com>
2014-03-03 23:13:27 -0600

    at least we now send the message

--- src/main/java/com/rsm/clients/handlers/TimestampSecondsCommandEncoder.java
+++ src/main/java/com/rsm/clients/handlers/TimestampSecondsCommandEncoder.java
@@ -1,7 +1,9 @@
 package com.rsm.clients.handlers;
 
+import com.rsm.message.nasdaq.itch.v4_1.MessageHeader;
 import com.rsm.message.nasdaq.itch.v4_1.TimestampSecondsCommand;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import io.netty.handler.codec.MessageToMessageEncoder;
@@ -9,6 +11,7 @@ import org.apache.log4j.Logger;
 import uk.co.real_logic.sbe.codec.java.DirectBuffer;
 
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.util.List;
 
 /**
@@ -23,12 +26,39 @@ public class TimestampSecondsCommandEncoder extends MessageToMessageEncoder<Time
     protected void encode(ChannelHandlerContext ctx, TimestampSecondsCommand msg, List<Object> out) throws Exception {
         logger.info("Sending some stuff");
 
-        msg.streamHeader().timestampNanos(6).id(1);
-        msg.payload().seconds(6);
-        msg.wrapForEncode(new DirectBuffer(ctx.alloc().buffer().nioBuffer()), 0);
+        final ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4096);
+
+        final DirectBuffer buf = new DirectBuffer(byteBuffer);
+
+        int bufferOffset = 0;
+        int encodingLength = 0;
+
+//        msg.streamHeader().timestampNanos(6).id(1);
+//        msg.payload().seconds(6);
+//        msg.wrapForEncode(buf, 0);
+        MessageHeader messageHeader = new MessageHeader();
+
+        messageHeader.wrap(buf, bufferOffset, 0)
+                .blockLength(msg.sbeBlockLength())
+                .templateId(msg.sbeTemplateId())
+                .schemaId(msg.sbeSchemaId())
+                .version(msg.sbeSchemaVersion());
+
+        bufferOffset += messageHeader.size();
+        encodingLength += messageHeader.size();
+        encodingLength += encode(msg, buf, bufferOffset);
 
 
         out.add(msg);
+
+    }
+
+    private int encode(final TimestampSecondsCommand msg, final DirectBuffer directBuffer, final int bufferOfset) {
+        final int srcOffset = 0;
+
+        msg.wrapForEncode(directBuffer, bufferOfset).payload().seconds(100);
+
+        return msg.size();
 
     }
 }
