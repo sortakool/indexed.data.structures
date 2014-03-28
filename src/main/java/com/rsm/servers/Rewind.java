2588731e4c6afb6e2c070df58c26065c03975492
Ariel Himmelstern <arhimmel@gmail.com>
2014-03-27 22:40:07 -0500

    Rewind now joins the multicast event group

--- src/main/java/com/rsm/servers/Rewind.java
+++ src/main/java/com/rsm/servers/Rewind.java
@@ -1,18 +1,19 @@
 package com.rsm.servers;
 
-import java.net.InetSocketAddress;
-import java.net.StandardProtocolFamily;
-import java.net.StandardSocketOptions;
-import java.nio.channels.DatagramChannel;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import java.net.*;
+import java.nio.channels.*;
+import java.util.Enumeration;
 import java.util.Iterator;
 
 /**
  * Server that receives Mold Event and then adds sequences to the messages and forwards them
  */
 public class Rewind {
+
+    private static final Logger log = LogManager.getLogger(Rewind.class);
 
     public static final String EVENT_MULTICAST_IP = "FF02:0:0:0:0:0:0:4";
     public static final int EVENT_MULTICAST_PORT = 9001;
@@ -23,27 +24,36 @@ public class Rewind {
     private ServerSocketChannel tcpSocketChannel;
     private DatagramChannel eventChannel;
 
+    MembershipKey eventMembershipKey;
+
     public Rewind() throws Exception {
         Selector selector = Selector.open();
 
-        //create multicast connection
-        InetSocketAddress eventGroup = new InetSocketAddress(EVENT_MULTICAST_IP, EVENT_MULTICAST_PORT);
+        NetworkInterface networkInterface = getNetworkInterface();
+
+        //connect to the event group
         eventChannel = DatagramChannel.open(StandardProtocolFamily.INET6);
-        eventChannel.bind(null);
-//        eventChannel.setOption(StandardSocketOptions.IP_MULTICAST_IF, networkInterface);
-//        eventChannel.setOption(StandardSocketOptions.SO_SNDBUF, eventByteBuffer.capacity()*2);
+        eventChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
+        InetSocketAddress eventInetSocketAddress = new InetSocketAddress(EVENT_MULTICAST_PORT);
+        eventChannel.bind(eventInetSocketAddress);
+        eventChannel.setOption(StandardSocketOptions.IP_MULTICAST_IF, networkInterface);
+
+        InetAddress eventGroup = InetAddress.getByName(EVENT_MULTICAST_IP);
+        eventMembershipKey = eventChannel.join(eventGroup, networkInterface);
+
         eventChannel.configureBlocking(false);
         final SelectionKey readableSelectionKey = eventChannel.register(selector, SelectionKey.OP_READ);
+
 
         //create TCP connection
         tcpSocketChannel = ServerSocketChannel.open();
         tcpSocketChannel.setOption(StandardSocketOptions.SO_REUSEADDR, false);
         InetSocketAddress inetSocketAddress = new InetSocketAddress(TCP_REWIND_IP, TCP_REWIND_PORT);
-        tcpSocketChannel.bind(inetSocketAddress);
+        tcpSocketChannel.bind(null);
         tcpSocketChannel.configureBlocking(false);
 
         //we only care right now that we are ready to connect
-        final SelectionKey tcpRequest = tcpSocketChannel.register(selector, SelectionKey.OP_CONNECT);
+        final SelectionKey tcpRequestSelectionKey = tcpSocketChannel.register(selector, 0);
 
         boolean active = true;
         while(active) {
@@ -55,11 +65,14 @@ public class Rewind {
                     SelectionKey selectionKey = iter.next();
                     iter.remove();
 
-                    if (tcpRequest.isConnectable()) {
+                    if (tcpRequestSelectionKey.isConnectable()) {
                         //we have been connected to and need to read in the request from
                         //the client requesting a rewind
                     } else if (readableSelectionKey.isReadable()) {
                         //we got a message and should save it to our binary file
+                        log.info("got something");
+
+
                     }
                 }
             }
@@ -67,6 +80,20 @@ public class Rewind {
 
     }
 
+    private NetworkInterface getNetworkInterface() throws SocketException {
+        // Get the reference of a network interface
+        NetworkInterface networkInterface = null;
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while(networkInterfaces.hasMoreElements()) {
+            NetworkInterface nextNetworkInterface = networkInterfaces.nextElement();
+            log.info(nextNetworkInterface);
+            if(nextNetworkInterface.supportsMulticast()) {
+                networkInterface = nextNetworkInterface;
+//                break;
+            }
+        }
+        return networkInterface;
+    }
 
     public static void main(String[] args) throws Exception{
         Rewind rewind = new Rewind();
